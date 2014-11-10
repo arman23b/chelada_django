2993c68c83eb0bdf1f352f0291de663c50915dbc
arman23b <arman23b@gmail.com>
2014-11-10 17:46:59 -0500

    debugging acra

--- core/views.py
+++ core/views.py
@@ -157,7 +157,14 @@ def mobileGetCan(request, canName):
 
 
 def test(request):
+	user = request.user
 	data = request.POST
+	content = json.loads(data)
+	new_can = models.Cans.objects.create(name="TestCan", 
+										 owner=user, 
+										 content=content, 
+										 view_permission="public")
+	new_can.save()
 	return HttpResponse(json.dumps(data), content_type="application-json")
 
 ###################
