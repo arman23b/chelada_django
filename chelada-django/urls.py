9d2cc19dc6f5e99dbefd68fc35b75a41a7c7f819
arman23b <arman23b@gmail.com>
2015-01-27 18:12:34 -0500

    Put django-gcm urls back

--- chelada-django/urls.py
+++ chelada-django/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^acra/', include("acra.urls")),
-    # url(r'^', include('gcm.urls')),
+    url(r'^', include('gcm.urls')),
     url(r'^', include('core.urls')),
 )
